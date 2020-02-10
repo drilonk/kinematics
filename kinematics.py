810b077d4d41fb9e9ee710b7c90fcf7258f67adc
Drilon Kllokoqi <drilonkllokoqi@hotmail.com>
2020-02-10 23:22:16 +0100

    added little function

--- kinematics.py
+++ kinematics.py
@@ -1,3 +1,12 @@
 import numpy as np
 import pandas as pd
 
+class TranslatingCordinateSystem:
+
+    def __init__(self, r_a = None, v_a = None, a_a = None):
+        self.r_a = r_a
+        self.v_a = v_a
+        self.a_a = a_a
+
+    def translations(self):
+        r_b = self.r_a
